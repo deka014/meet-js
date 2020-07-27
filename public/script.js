5b61eb6c550fe8c259c48e05284f21a9661cdf84
deepjyoti deka <deepjyotideka8@gmail.com>
2020-07-27 17:18:21 +0000

    used heroku peer-server

--- public/script.js
+++ public/script.js
@@ -1,7 +1,16 @@
 const socket = io("/")
+const myPeer = new Peer({
+	host:'peerjs-server.herokuapp.com', 
+	secure:true, 
+	port:443})
+
+myPeer.on('open', function(id) {
+  console.log('My peer ID is: ' + id);
+});
+
 
 socket.emit("join-room" , roomId ,  10 )
 
 socket.on("user-connected" , userId =>{
 	console.log(userId)
-} )
+})
